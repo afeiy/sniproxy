e39b7eb6814724ffd12e26b59851e9207e6335ea
buxiaomo <95112082@qq.com>
2019-12-24 14:51:28 +0800

    fix sniproxy configure err

--- install.sh
+++ install.sh
@@ -2,14 +2,18 @@
 [ $(id -u) != 0 ] && (echo "Please run as root user!" && exit 1)
 IP=$(curl -s http://ifconfig.me)
 
-apt-get install sniproxy dnsmasq -y
+# dnsmasq
+apt-get install dnsmasq -y
 wget https://raw.githubusercontent.com/buxiaomo/sniproxy/master/dnsmasq.conf -O /etc/dnsmasq.conf
-wget https://raw.githubusercontent.com/buxiaomo/sniproxy/master/sniproxy.conf -O /etc/sniproxy.conf
-cat >> /etc/sniproxy.conf << EOF
-address=/netflix.com/${IP}
-address=/nflxvideo.net/${IP}
-EOF
+sed '/netflix.com/d' /etc/dnsmasq.conf && echo "address=/netflix.com/${IP}" >> /etc/dnsmasq.conf
+sed '/nflxvideo.net/d' /etc/dnsmasq.conf && echo "address=/nflxvideo.net/${IP}" >> /etc/dnsmasq.conf
 
+# sniproxy
+apt-get install sniproxy -y
+wget https://raw.githubusercontent.com/buxiaomo/sniproxy/master/sniproxy.conf -O /etc/sniproxy.conf
+sed -i "s/^#DAEMON_ARGS/DAEMON_ARGS/g" /etc/default/sniproxy
+
+# nginx
 curl -s http://nginx.org/keys/nginx_signing.key | apt-key add -
 cat > /etc/apt/sources.list.d/nginx.list << EOF
 deb http://nginx.org/packages/$(. /etc/os-release; echo "$ID")/ $(lsb_release -cs) nginx
@@ -21,6 +25,7 @@ wget https://raw.githubusercontent.com/buxiaomo/sniproxy/master/nginx.conf -O /e
 sed -i "s/xxx.xxx.xxx.xxx/${Client}/g" /etc/nginx/nginx.conf
 rm -rf /etc/nginx/conf.d/*
 
+# systemd
 systemctl restart sniproxy dnsmasq nginx
 systemctl enable sniproxy dnsmasq nginx
 
